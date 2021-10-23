ee1ae6031da0bc6098d2d85c5e6074d79253394f
Valentin <valentin.kil.2002@gmail.com>
2021-10-23 14:53:05 +0300

    working 3C

--- Lab3/C/main.cpp
+++ Lab3/C/main.cpp
@@ -6,30 +6,65 @@ using namespace std;
 #define s second
 #define mp make_pair
 #define ll long long
- 
+
 #define MAXN 200005
 #define INF 1000000009
 #define MOD 1000000007
- 
 
-void solve(){
-	
+void countSort(string arr[], int size, int d) {
+    int count[26] = {};
+
+    for (int i = 0; i < size; i++) {
+        count[arr[i][d] - 'a']++;
+    }
+    for (int i = 1; i < 26; i++) {
+        count[i] += count[i - 1];
+    }
+
+    string out[size];
+    for (int i = size - 1; i >= 0; i--) {
+        out[(--count[arr[i][d] - 'a'])] = arr[i];
+    }
+
+    for (int i = 0; i < size; i++) {
+		arr[i] = out[i];
+	}
 }
 
-int main(){
-	ios_base::sync_with_stdio(0);
+void radixSort(string arr[], int size, int m, int op) {
+    for (int i = m - 1; i >= m - op; i--) {
+        countSort(arr, size, i);
+    }
+}
+
+void solve() {	
+    int n, m, k;
+    cin >> n >> m >> k;
+    string arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    radixSort(arr, n, m, k);
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << endl;
+    cout << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
-	int tests = 1;
+    int tests = 1;
+
+    freopen("radixsort.in", "r", stdin);
+    freopen("radixsort.out", "w", stdout);
 
 #ifdef LOCAL
-	bool a;
-	a = freopen("in.data", "r", stdin);
-	a = freopen("out.data", "w", stdout);
-	cin >> tests;
+    cin >> tests;
 #endif
 
-	while(tests--)
-		solve();
+    while (tests--)
+        solve();
 }
