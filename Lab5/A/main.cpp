0d11fe202a9b0cc10358d39a9aca11319d7328e6
Valentin <valentin.kil.2002@gmail.com>
2021-11-17 18:24:27 +0300

    add 5A and 5B solved

--- Lab5/A/main.cpp
+++ Lab5/A/main.cpp
@@ -10,26 +10,66 @@ using namespace std;
 #define MAXN 200005
 #define INF 1000000009
 #define MOD 1000000007
- 
 
-void solve(){
-	
+void solve() {
+    vector<int> parent;
+    vector<vector<int> > kids;
+    int n;
+    cin >> n;
+
+	if (!n) {
+		cout << 0 << '\n';
+		return;
+	}
+
+    parent.resize(n, -1);
+    kids.resize(n);
+    for (int i = 0; i < n; i++) {
+        int v, k[2];
+        cin >> v >> k[0] >> k[1];
+        for (int j = 0; j < 2; j++) {
+            if (k[j]) {
+                kids[i].pb(k[j] - 1);
+                parent[k[j] - 1] = i;
+            }
+        }
+    }
+
+    int root = 0;
+    for (; parent[root] != -1; root++);
+
+    queue<int> q;
+    q.push(root);
+    int level = 0;
+    while (!q.empty()) {
+        int last = q.back();
+        int cur = -1;
+        while (cur != last) {
+            cur = q.front();
+            q.pop();
+            for (int i = 0; i < kids[cur].size(); i++) {
+                q.push(kids[cur][i]);
+            }
+        }
+        level++;
+    }
+
+    cout << level << '\n';
 }
 
-int main(){
-	ios_base::sync_with_stdio(0);
+int main() {
+    ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
-	int tests = 1;
+    int tests = 1;
 
+    freopen("height.in", "r", stdin);
+    freopen("height.out", "w", stdout);
 #ifdef LOCAL
-	bool a;
-	a = freopen("in.data", "r", stdin);
-	a = freopen("out.data", "w", stdout);
-	cin >> tests;
+    cin >> tests;
 #endif
 
-	while(tests--)
-		solve();
+    while (tests--)
+        solve();
 }
--- Lab5/B/main.cpp
+++ Lab5/B/main.cpp
@@ -6,30 +6,66 @@ using namespace std;
 #define s second
 #define mp make_pair
 #define ll long long
- 
+
 #define MAXN 200005
 #define INF 1000000009
 #define MOD 1000000007
- 
 
-void solve(){
-	
+vector<pair<int, int> > kids;
+vector<int> value;
+
+bool dfs(int k, int mn, int mx) {
+	if (k == -1) 
+		return 1;
+	bool ans = 1;
+	if (kids[k].f != -1) {
+		ans = ans && (value[kids[k].f] > mn);
+		ans = ans && (value[kids[k].f] < value[k]);		
+	}
+	if (kids[k].s != -1) {
+		ans = ans && (value[kids[k].s] > value[k]);
+		ans = ans && (value[kids[k].s] < mx);
+	}
+
+	return ans && dfs(kids[k].f, mn, value[k]) && dfs(kids[k].s, value[k], mx);
 }
 
-int main(){
-	ios_base::sync_with_stdio(0);
+void solve() {
+    int n;
+    cin >> n;    
+
+    if (!n) {
+        cout << "YES" << '\n';
+        return;
+    }
+
+	value.resize(n);
+    kids.resize(n);
+    for (int i = 0; i < n; i++) {
+        int v, k[2];
+        cin >> v >> k[0] >> k[1];
+        kids[i] = mp(k[0] - 1, k[1] - 1);
+        value[i] = v;
+    }
+
+	bool ans = dfs(0, -INF, INF);
+
+    cout << (ans ? "YES" : "NO") << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
-	int tests = 1;
+    int tests = 1;
 
+    freopen("check.in", "r", stdin);
+    freopen("check.out", "w", stdout);
 #ifdef LOCAL
-	bool a;
-	a = freopen("in.data", "r", stdin);
-	a = freopen("out.data", "w", stdout);
-	cin >> tests;
+    cin >> tests;
 #endif
 
-	while(tests--)
-		solve();
+    while (tests--)
+        solve();
 }
