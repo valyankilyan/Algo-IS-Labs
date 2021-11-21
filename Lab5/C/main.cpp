3989aa9a50a1e6502f97e36d27e7b062bc9611f5
valyankilyan <valentin.kil.2002@gmail.com>
2021-11-21 22:30:21 +0300

    add working 5C

--- Lab5/C/main.cpp
+++ Lab5/C/main.cpp
@@ -30,98 +30,51 @@ class BSTSimple {
         root = NULL;
     }
 
-    void insert(int in) {
-        if (exists(in)) {
-            return;
-        }
-        cerr << "insert(" << in << ") \n";
-        if (root == NULL) {
-            Node *tmp = new Node(in);            
-            root = tmp;            
-        } else {
-            Node *cur = root;
-            while (!put_if_right_node(cur, in)) {
-                cur = next_node(cur, in);
-            }
-        }
-        print();
+    void insert(int value) {
+        root = insertNode(root, value);
     }
 
-    // void remove(int in) {
-    //     cerr << "remove(" << in << ") \n";
-    //     Node *node = find_node(in);
-    //     cerr << "node->value = " << node->value << endl;
-    //     if (node->left == NULL && node->right == NULL) {
-    //         node = NULL;
-    //         delete node;            
-    //     } else if (node->left == NULL) {
-    //         // Node *tmp = node->right;
-    //         node->value = node->right->value;
-    //         node->left = node->right->left;
-    //         node->right = node->right->right;            
-    //     } else if (node->right == NULL) {
-    //         // Node *tmp = node->left;
-    //         node->value = node->left->value;
-    //         node->right = node->left->right;
-    //         node->left = node->left->left;
-    //     } else {
-    //         int tmp = min_node(node->right)->value;
-    //         remove(tmp);
-    //         node->value = tmp;
-    //     }
-    //     print();
-    // }
-
-    void remove(int in) {
-        cerr << "remove(" << in << ")\n";
-        root = removeNode(root, in);
-        print();
+    void remove(int value) {
+        root = removeNode(root, value);
     }
 
-    bool exists(int in) {
-        Node *search = find_node(in);
-        cerr << "exist(" << in << ") = " << (search != NULL) << "\n";
-        print();
+    bool exists(int value) {
+        Node *search = find_node(root, value);
         return search != NULL;
     }
 
     int next(int value) {
         Node *tmp = find_next_node(root, value);
         int ans = tmp == NULL ? -INF : tmp->value;
-        cerr << "next(" << value << ") = " << ans << '\n';
-        print();
         return ans;
     }
 
     int prev(int value) {
         Node *tmp = find_prev_node(root, value);
         int ans = tmp == NULL ? -INF : tmp->value;
-        cerr << "prev(" << value << ") = " << ans << '\n';
-        print();
         return ans;
     }
 
-    void print() {        
-        // cerr << "printing bst\n";
-		vector<vector<Node*>> v;
-        vector <Node*> f_v;
+    void print() {
+        vector<vector<Node *>> v;
+        vector<Node *> f_v;
         f_v.pb(root);
-		v.pb(f_v);        
+        v.pb(f_v);
         bool neot = true;
-		bool is_not_end = false;
-		while (neot) {            
+        bool is_not_end = false;
+        while (neot) {
             neot = 0;
-            vector <Node*> new_line;
-            int last = v.size() - 1;            
-            for (int i = 0; i < v[last].size(); i++) {                
+            vector<Node *> new_line;
+            int last = v.size() - 1;
+            for (int i = 0; i < v[last].size(); i++) {
                 if (v[last][i] == NULL) {
                     new_line.pb(NULL);
-                    new_line.pb(NULL);                                        
+                    new_line.pb(NULL);
                 } else {
                     new_line.pb(v[last][i]->left);
                     new_line.pb(v[last][i]->right);
                     neot = (v[last][i]->left != NULL) || neot;
-                    neot = (v[last][i]->right != NULL) || neot;                    
+                    neot = (v[last][i]->right != NULL) || neot;
                 }
             }
             if (neot)
@@ -129,22 +82,36 @@ class BSTSimple {
         }
 
         int space = (1 << (v.size() - 1)) - 1;
-        for (auto x: v) {            
-            for (auto y: x) {                
+        for (auto x : v) {
+            for (auto y : x) {
                 cerr << string(space, ' ');
                 if (y != NULL)
                     cerr << y->value << " ";
-                else 
+                else
                     cerr << "- ";
-                cerr << string(space, ' ');            
+                cerr << string(space, ' ');
             }
             space = space >> 1;
             cerr << "\n";
-        }		
+        }
     }
 
    private:
-    Node* removeNode(Node *cur, int value) {
+    Node *insertNode(Node *cur, int value) {
+        if (cur == NULL) {
+            Node *tmp = new Node(value);
+            return tmp;
+        }
+        if (cur->value < value) {
+            cur->right = insertNode(cur->right, value);
+        }
+        if (value < cur->value) {
+            cur->left = insertNode(cur->left, value);
+        }
+        return cur;
+    }
+
+    Node *removeNode(Node *cur, int value) {
         if (cur == NULL) {
             return NULL;
         }
@@ -157,17 +124,17 @@ class BSTSimple {
                 return NULL;
             }
             if (cur->left == NULL) {
-                Node* tmp = cur->right;
+                Node *tmp = cur->right;
                 delete cur;
                 return tmp;
             }
             if (cur->right == NULL) {
-                Node* tmp = cur->left;
+                Node *tmp = cur->left;
                 delete cur;
                 return tmp;
             }
 
-            Node* tmp = min_node(cur->right);
+            Node *tmp = min_node(cur->right);
             cur->value = tmp->value;
             cur->right = removeNode(cur->right, tmp->value);
         }
@@ -175,46 +142,30 @@ class BSTSimple {
         return cur;
     }
 
-    Node* min_node(Node *cur) {
+    Node *min_node(Node *cur) {
         if (cur->left != NULL)
             return min_node(cur->left);
         return cur;
     }
 
-    bool put_if_right_node(Node *cur, int in) {
-        // cerr << cur->value << " " << in << endl;
-        if (cur->value < in && cur->right == NULL) {
-            Node *tmp = new Node(in);
-            cur->right = tmp;
-            return true;
+    Node *find_node(Node *cur, int value) {
+        if (cur == NULL) {
+            return NULL;
         }
-        if (in < cur->value && cur->left == NULL) {
-            Node *tmp = new Node(in);
-            cur->left = tmp;
-            return true;
+        if (cur->value < value) {
+            return find_node(cur->right, value);
         }
-        return false;
-    }
-
-    Node *next_node(Node *cur, int in) {
-        if (cur->value < in) {
-            return cur->right;
-        }
-        if (in < cur->value) {
-            return cur->left;
-        }
-        return cur;
-    }
-
-    Node *find_node(int in) {
-        Node *cur = root;
-        while (cur != NULL && cur->value != in) {
-            cur = next_node(cur, in);
+        if (value < cur->value) {
+            return find_node(cur->left, value);
         }
         return cur;
     }
 
     Node *find_next_node(Node *cur, int value) {
+        if (cur == NULL) {
+            return cur;
+        }
+
         if (cur->value <= value) {
             if (cur->right == NULL) {
                 return NULL;
@@ -230,12 +181,16 @@ class BSTSimple {
                 return cur;
             } else {
                 return tmp;
-            }            
+            }
         }
         return NULL;
     }
 
     Node *find_prev_node(Node *cur, int value) {
+        if (cur == NULL) {
+            return cur;
+        }
+
         if (value <= cur->value) {
             if (cur->left == NULL) {
                 return NULL;
@@ -263,8 +218,11 @@ void solve() {
     while (cin >> request) {
         int x;
         cin >> x;
-        if (request == "insert") {            
-            bst.insert(x);                        
+#ifdef CERR
+        cerr << request << " " << x << endl;
+#endif
+        if (request == "insert") {
+            bst.insert(x);
         } else if (request == "delete") {
             bst.remove(x);
         } else if (request == "exists") {
@@ -284,6 +242,9 @@ void solve() {
                 cout << "none\n";
             }
         }
+#ifdef CERR
+        bst.print();
+#endif
     }
 }
 
@@ -297,7 +258,7 @@ int main() {
     freopen("bstsimple.in", "r", stdin);
     freopen("bstsimple.out", "w", stdout);
 
-#ifdef LOCAL    
+#ifdef LOCAL
     cin >> tests;
 #endif
 
