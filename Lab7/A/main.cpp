77af2b491e5b1274635e8291f22bfcddc30bcff8
valyankilyan <valentin.kil.2002@gmail.com>
2021-12-18 18:13:58 +0300

    Lab7 A done

--- Lab7/A/main.cpp
+++ Lab7/A/main.cpp
@@ -10,26 +10,64 @@ using namespace std;
 #define MAXN 200005
 #define INF 1000000009
 #define MOD 1000000007
- 
 
-void solve(){
-	
+vector<int> B;
+vector<pair<int, pair<int, int>>> tree;
+
+int dfs(int v) {
+    int l = 0, r = 0;
+    if (tree[v].s.f) {
+        l = dfs(tree[v].s.f);
+    }
+    if (tree[v].s.s) {
+        r = dfs(tree[v].s.s);
+    }
+
+    B[v] = r - l;
+    return max(l, r) + 1;
 }
 
-int main(){
-	ios_base::sync_with_stdio(0);
+void solve() {
+    int n;
+    cin >> n;
+    B.resize(n + 1);
+    tree.resize(n + 1);
+    for (int i = 1; i <= n; i++) {
+        int k, l, r;
+        cin >> k >> l >> r;
+        tree[i].s.f = l;
+        tree[i].s.s = r;
+        tree[l].f = i;
+        tree[r].f = i;
+    }
+    int root;
+    for (int i = 1; i <= n; i++) {
+        if (tree[i].f == 0) {
+            root = i;
+            break;
+        }
+    }
+
+    dfs(root);
+
+    for (int i = 1; i <= n; i++) {
+        cout << B[i] << "\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
-	int tests = 1;
+    int tests = 1;
 
+    freopen("balance.in", "r", stdin);
+    freopen("balance.out", "w", stdout);
 #ifdef LOCAL
-	bool a;
-	a = freopen("in.data", "r", stdin);
-	a = freopen("out.data", "w", stdout);
-	cin >> tests;
+    cin >> tests;
 #endif
 
-	while(tests--)
-		solve();
+    while (tests--)
+        solve();
 }
