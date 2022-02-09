f4a2842ff10f810c047681e251199b85f8caddac
valyankilyan <valentin.kil.2002@gmail.com>
2022-02-09 15:37:21 +0300

    new semester

---
+++ 2sem/Lab8/B/main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define f first
+#define s second
+#define mp make_pair
+#define ll long long
+ 
+#define MAXN 200005
+#define INF 1000000009
+#define MOD 1000000007
+ 
+
+void solve(){
+	
+}
+
+int main(){
+	ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+	int tests = 1;
+
+#ifdef LOCAL
+	bool a;
+	a = freopen("in.data", "r", stdin);
+	a = freopen("out.data", "w", stdout);
+	cin >> tests;
+#endif
+
+	while(tests--)
+		solve();
+}
