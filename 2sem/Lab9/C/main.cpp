3966702fa7a5f23d8cba21a4935ad7570f3d07e3
valyankilyan <valentin.kil.2002@gmail.com>
2022-03-01 17:49:07 +0300

    9C solved

--- 2sem/Lab9/C/main.cpp
+++ 2sem/Lab9/C/main.cpp
@@ -11,9 +11,63 @@ using namespace std;
 #define INF 1000000009
 #define MOD 1000000007
  
+vector <vector <int> > v;
+vector <int> part;
 
 void solve(){
-	
+	v.clear();
+	part.clear();
+	int n, m;
+	cin >> n >> m;
+	v.resize(n);
+	part.resize(n, -1);
+	for (int i = 0; i < m; i++) {
+		int fr, to;
+		cin >> fr >> to;
+		v[fr-1].pb(to-1);
+		v[to-1].pb(fr-1);
+	}
+
+	bool ans = 1;
+	// cerr << "n = " << n << endl;
+	for (int i = 0; i < n && ans; i++) {
+	// for (int i = 0; i < n; i++) {
+		if (part[i] != -1) {
+			continue;
+		}
+		// cerr << "OK! " << i << "\n";
+		queue <int> q;
+		part[i] = 1;
+		q.push(i);
+
+		while (!q.empty() && ans) {		
+		// while (!q.empty()) {		
+			int cur = q.front();
+			cerr << cur+1 << " -> ";
+			q.pop();
+			for (int j = 0; j < v[cur].size(); j++) {
+				if (part[v[cur][j]] == -1) {
+					part[v[cur][j]] = !part[cur];
+					q.push(v[cur][j]);
+					// cerr << " {push " << v[cur][j] + 1 << "} ";
+				} else if (part[v[cur][j]] == part[cur]) {
+					ans = 0;
+					break;
+				}
+			}
+		}
+	}
+	cerr << endl;
+
+	// for (int i = 0; i < n; i++) {
+	// 	cout << i+1 << ": " << part[i] << endl;
+	// }
+
+	// for (int i = 0; i < n; i++) {
+	// 	cerr << i+1 << " " << was[i] << endl;
+	// }
+
+	cout << (ans ? "YES\n" : "NO\n");
 }
 
 int main(){
@@ -23,10 +77,9 @@ int main(){
 
 	int tests = 1;
 
+	freopen("bipartite.in", "r", stdin);
+	freopen("bipartite.out", "w", stdout);
 #ifdef LOCAL
-	bool a;
-	a = freopen("in.data", "r", stdin);
-	a = freopen("out.data", "w", stdout);
 	cin >> tests;
 #endif
 
